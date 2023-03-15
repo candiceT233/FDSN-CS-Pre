25e1e9bd3a47d36d21478397b2fc1b6c1fd4e06a
KarthikKaiplody <logontokarthikk@gmail.com>
2023-03-15 13:50:16 -0500

    Added data-science template

---
+++ FDSN-CS/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Illinois Tech SCS lab collaboration with FDSN department on human nutrition studies.',
+    author='SCS-LAB',
+    license='',
+)
